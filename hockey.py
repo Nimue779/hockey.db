c9303fe633ef0e4660b77a2d398947bb0c251a22
Nimue779 <24049@burnside.school.nz>
2026-05-26 09:50:09 +1200

    test

--- hockey.py
+++ hockey.py
@@ -1,7 +1,26 @@
 import sqlite3
 
-db = sqlite3.connect('hockey.db')
-cursor = db.cursor()
-show_all_players = 'SELECT * FROM players;'
+DATABASE = 'hockey.db'
 
-db.close()
+with sqlite3.connect(DATABASE) as db:
+
+    cursor = db.cursor()
+    show_all_players = 'SELECT * FROM players;'
+    cursor.execute(show_all_players)
+    results = cursor.fetchall()
+    print(results)
+    # Print the data so you can actually see it
+    for player in results:
+        print(f"Player: {player[1]} Team: {player[2]}")
+
+with sqlite3.connect(DATABASE) as db:
+
+    cursor = db.cursor()
+    top20 = 'SELECT * FROM players ORDER BY points DESC LIMIT 20;'
+    cursor.execute(top20)
+    results = cursor.fetchall()
+    print(results)
+    # Print the data so you can actually see it
+    for top_20 in results:
+        print(f"Player: {player[1]}, Team: {player[2]}, Points: {player[8]}")
+        print('hello')
