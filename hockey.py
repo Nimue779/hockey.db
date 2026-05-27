8be4633399a40cbe4796c784bc3d5469cf78a9c9
Nimue779 <24049@burnside.school.nz>
2026-05-27 14:28:28 +1200

    tidied up my code, created functions

--- hockey.py
+++ hockey.py
@@ -1,47 +1,29 @@
+# imports
 import sqlite3
 
+# constants and variables
 DATABASE = 'hockey.db'
 
-print('Welcome to my Hockey Database')
-print('1. Print all player and their team')
-print('2. Print all players alphabetically and their stats')
-print('3. Print top 20 players')
-# this query prints all players and their team, no extra stats
-with sqlite3.connect(DATABASE) as db:
 
+# functions
+def print_all_players():
+    '''print all players nicely'''
+    db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
-    show_all_players = 'SELECT * FROM players;'
-    cursor.execute(show_all_players)
+    sql = 'SELECT * FROM players'
+    cursor.execute(sql)
     results = cursor.fetchall()
-    print(results)
-    # Print the data so you can actually see it
+    print('All players and teams')
+    # loop through the results
     for player in results:
-        print(f"Player: {player[1]} Team: {player[2]}")
+        print(f"Player:{player[1]:<30} Team:{player[2]}")
+    # loop finishes here
+    db.close()
 
-# this query shows the top 20 players in the league based on their points
-with sqlite3.connect(DATABASE) as db:
+# main code
+while True:
+    user_input = input("\nWhat would you like to do?\n1Print all players \n2Exit \n")
 
-    cursor = db.cursor()
-    top20 = 'SELECT * FROM players ORDER BY points DESC LIMIT 20;'
-    cursor.execute(top20)
-    results = cursor.fetchall()
-    print(results)
-    # Print the data so you can actually see it
-    for player in results:
-        print(f"Player: {player[1]}, Team: {player[2]}, Points: {player[8]}")
+    if user_input == '1':
+        print_all_players()
 
-# this query will print all players alphabetically. this includes player,
-# team, what hand they shoot with,
-# position, games played, points, the +/-/ and penalty minutes
-with sqlite3.connect(DATABASE) as db:
-
-    cursor = db.cursor()
-    alphabetical = 'SELECT * FROM players ORDER BY name ASC;'
-    cursor.execute(alphabetical)
-    results = cursor.fetchall()
-    print(results)
-    # Print the data so you can actually see it
-    for player in results:
-        print(f"Player: {player[1]}, Team: {player[2]}, Shoots: {player[3]},\
-Position: {player[4]}, Games played: {player[5]},\
-Points: {player[8]},+/-: {player[9]}, Penalty Minutes: {player[10]}")
